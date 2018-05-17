74363a8be789527e42fc5a54c050d399659e2a22
Sam G. Zun <zunsamuel97@gmail.com>
2018-05-17 18:03:39 -0500

    Pushing Current Version

---
+++ Intro_Page/assets/javascript/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    $("#firstName").hover(function () {
+        $(this).text('About')
+    });
+    $("#middleName").hover(function () {
+        $(this).text('Portfolio')
+    });
+    $("#surname").hover(function () {
+        $(this).text('Contact')
+    });
+
+})
